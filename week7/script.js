0825c5de60d160da411d06fe23c43e2ac962f55b
hamishrmit <hamishdigitalmedia@gmail.com>
2026-05-01 11:58:44 +1000

    week 7 almost done

---
+++ week7/script.js
@@ -0,0 +1,52 @@
+// first get access to the audio element so that we can control it from here
+const airportAudio = document.querySelector("#airport-audio");
+console.log(airportAudio);
+
+// let us access the video too
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+// similarly access the play button
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+
+playButton.addEventListener("click", playAudio);
+
+function playAudio() {
+  //   airportAudio.play();
+  myVideo.play();
+  msg.textContent = "audio is playing";
+}
+
+// similarly access the pause button
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+
+pauseButton.addEventListener("click", pauseAudio);
+
+function pauseAudio() {
+  //   airportAudio.pause();
+  myVideo.pause();
+  msg.textContent = "audio is paused";
+}
+
+// similarly access the pop button
+const popAudio = document.querySelector("#pop-audio");
+console.log(popAudio);
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+popButton.addEventListener("click", makeItPop);
+
+function makeItPop() {
+  popAudio.play();
+  msg.textContent = "popping...";
+}
+
+// didn't see full code
+// const body = document.querySelector("body");
+// body.addEventListener("keydown", playVideo);
+
+// function playVideo() {
+//     myVideo.play();
+// }
